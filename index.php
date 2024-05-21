b1630b68a4ed4f7e87cb553c12b8399f7b0a62d6
palmuccivalerio <palmuccivalerio@gmail.com>
2024-05-21 14:04:38 +0200

    add:array

---
+++ index.php
@@ -0,0 +1,72 @@
+<?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+
+    foreach ($hotels as $hotel) {
+        echo "<div>Nome:                {$hotel['name']}    </div>";
+        echo "<div>Descrizione:         {$hotel['description']} </div>";
+        echo "<div>Parcheggio:          {$hotel['parking']}      </div>";
+        echo "<div>Voto:                {$hotel['vote']}    </div>";
+        echo "<div>Distanza dal centro: {$hotel['distance_to_center']}  </div>";
+        echo "<hr>";
+    }
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+</head>
+<body>
+    <h2>ciaooooo</h2>
+
+
+</body>
+</html>
